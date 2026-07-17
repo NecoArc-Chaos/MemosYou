cf2bff6ceae3eafe91511b9c85a692e0116db408
Neco-ArcChaos <acceleratoor0123@gmail.com>
2026-07-17 12:32:53 +0000

    feat: add OkHttp logging interceptor for debugging API responses

--- app/src/main/java/xyz/nachaos/memosyou/data/module/NetworkModule.kt
+++ app/src/main/java/xyz/nachaos/memosyou/data/module/NetworkModule.kt
@@ -5,7 +5,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.JavaNetCookieJar
+import okhttp3.LoggingInterceptor
 import okhttp3.OkHttpClient
+import xyz.nachaos.memosyou.BuildConfig
 import java.net.CookieManager
 import java.net.CookiePolicy
 
@@ -17,8 +19,18 @@ object NetworkModule {
     fun provideOkHttpClient(): OkHttpClient {
         val cookieManager = CookieManager()
         cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL)
-        return OkHttpClient.Builder()
+        
+        val builder = OkHttpClient.Builder()
             .cookieJar(JavaNetCookieJar(cookieManager))
-            .build()
+        
+        // Only enable logging in debug builds
+        if (BuildConfig.DEBUG) {
+            val loggingInterceptor = LoggingInterceptor().apply {
+                level = LoggingInterceptor.Level.BODY
+            }
+            builder.addInterceptor(loggingInterceptor)
+        }
+        
+        return builder.build()
     }
 }
