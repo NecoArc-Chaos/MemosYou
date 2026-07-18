6df6ecc80659c8e6c9e02caf64f0a59d109e189b
Neco-ArcChaos <acceleratoor0123@gmail.com>
2026-07-18 01:20:18 +0000

    fix: correct import order in UserProfileHeader, add painterResource imports

--- app/src/main/java/xyz/nachaos/memosyou/ui/component/SideDrawer.kt
+++ app/src/main/java/xyz/nachaos/memosyou/ui/component/SideDrawer.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
--- app/src/main/java/xyz/nachaos/memosyou/ui/component/UserProfileHeader.kt
+++ app/src/main/java/xyz/nachaos/memosyou/ui/component/UserProfileHeader.kt
@@ -1,6 +1,6 @@
-import android.util.Log
 package xyz.nachaos.memosyou.ui.component
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
